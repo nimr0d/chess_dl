8074707856103af66b3d336a40bd1587157bd81b
Nimrod <contact@nimrodh.com>
2024-01-12 14:48:37 -0500

    Add nix flake and vendor tls support with rustls

---
+++ nix/shell.nix
@@ -0,0 +1,18 @@
+{
+  mkShell,
+  callPackage,
+  rustc,
+  rust-analyzer,
+  rustfmt,
+  clippy,
+  ...
+}:
+mkShell {
+  inputsFrom = [(callPackage ./. {})];
+  buildInputs = [
+    rustc
+    rust-analyzer
+    rustfmt
+    clippy
+  ];
+}
